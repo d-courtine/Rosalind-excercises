5f8bfe021d29761e3fde51917091afb190be3953
Damien Courtine <damien.courtine@gmail.com>
2021-02-22 20:08:14 +0100

    MEME done

--- bioinfo_armory/MEME/run.sh
+++ bioinfo_armory/MEME/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -v $(pwd):/home/meme --user $(id -u):$(id -g) memesuite/memesuite meme -nmotifs 1 -dna test.fa
+docker run -v $(pwd):/home/meme --user $(id -u):$(id -g) memesuite/memesuite meme ${1} -protein -oc . -nostatus -time 14400 -mod zoops -nmotifs 3 -objfun classic -markov_order 0 -oc meme_res
