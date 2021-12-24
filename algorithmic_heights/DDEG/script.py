bdbde8a3b9cb3c50f7acbe64b2e95621c63b7ff0
Damien Courtine <damien.courtine@uca.fr>
2021-12-24 09:44:55 +0100

    DDEG solved
    Signed-off-by: Damien Courtine <damien.courtine@uca.fr>

--- algorithmic_heights/DDEG/script.py
+++ algorithmic_heights/DDEG/script.py
@@ -3,36 +3,33 @@
 import networkx as nx
 
 #Read the graph with a package
-with open('test.txt', 'rb') as graph:
-# with open('rosalind_deg.txt', 'rb') as graph:
+# with open('test.txt', 'rb') as graph:
+with open('rosalind_ddeg.txt', 'rb') as graph:
     graph_stats = next(graph).decode("utf-8").rstrip().split(' ') ## [0] = nb nodes, [1] = nb edges
     myGraph = nx.read_edgelist(graph)
 
 """
-Return: An array D[1..n] where D[i] is the sum of the degrees of i's neighbors.
-
-1. browse the graph, and for each node, populate the dictionnary: {NodeID:[neighborID]}
-2. browse the dictionnary, for each NodeID:
-    - Get their neighbour
-    - Sum their degrees.
+Return: An array D[1..n] where D[i] is the sum of the degrees of it's neighbour.
+    1. browse the graph, and for each node, populate the dictionnary: {NodeID:[neighbourID]}
+    2. browse the dictionnary, for each NodeID:
+        - Get their neighbour
+        - Sum their degrees.
 """
 
 #setup the dictionary:
 result = dict()
-for node in range(1, len(myGraph.nodes()) + 1):
+for node in range(1, int(graph_stats[0]) + 1):
     result[str(node)] = list()
 
-# Is there unconnected nodes?
-if graph_stats[0] != len(result):
-    print("There is at least a single node! %s %s" %(graph_stats[0], len(result)))
+# For each node, record it neighbour, in both direction
+for edge in myGraph.edges():
+    result[edge[0]].append(edge[1])
+    result[edge[1]].append(edge[0])
 
-print(myGraph.nodes())
-
-# for edge in myGraph.edges():
-    # for node in edge:
-    #     result[node] += 1
-#
-# [print(x, end = " ") for x in list(result.values())]
-# print()
-
-# print(result)
+# For each node, get the sum of the degrees of it's neighbour
+for node, neighbour_list in result.items():
+    degrees = 0
+    for neighbour in neighbour_list:
+        degrees += len(result[neighbour])
+    print(degrees, end = ' ')
+print()
